2edf3f3af62080269996aac1421504395fff5ecb
lishaoqiang <lishaoqiang@zhaomoapp.com>
2023-08-07 11:47:54 +0800

    add

--- string/str_snake_to_camel.go
+++ string/str_snake_to_camel.go
@@ -8,6 +8,34 @@ func StrSnakeToCamel(s string) string {
 	for i := 0; i <= num; i++ {
 		d := s[i]
 		//首字母大写
+		// if i == 0 && d >= 'a' && d <= 'z' {
+		// 	d = d - 32
+		// }
+		if !k && d >= 'A' && d <= 'z' {
+			k = true
+		}
+		if d >= 'a' && d <= 'z' && (j || !k) {
+			d = d - 32
+			j = false
+			k = true
+		}
+		if k && string(d) == "_" && num > i && s[i+1] >= 'a' && s[i+1] <= 'z' {
+			j = true
+			continue
+		}
+		data = append(data, d)
+	}
+	return string(data[:])
+}
+
+func StrSnakeToCamelFirstUp(s string) string {
+	data := make([]byte, 0, len(s))
+	j := false
+	k := false
+	num := len(s) - 1
+	for i := 0; i <= num; i++ {
+		d := s[i]
+		//首字母大写
 		if i == 0 && d >= 'a' && d <= 'z' {
 			d = d - 32
 		}
